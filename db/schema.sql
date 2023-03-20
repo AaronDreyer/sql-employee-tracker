4932e84563a37eccb382758aa6afe27f4e5140ca
AaronDreyer <aaronottaway@gmail.com>
2023-03-20 12:07:46 -0400

    Schema.sql fix
    I fixed a couple of items within the schema.sql. First I DROP TABLE IF EXIST the each table to present them. The i removed unecessary NOT NULL from some of the table requirements. If an item is auot incremented, there is no need to add NOT NULL because theres not input. Also I removed the FORERIGN KEY and referencial data as this establishes basic structure while the inquire prompts will add them data themselves.

--- db/schema.sql
+++ db/schema.sql
@@ -1,35 +1,29 @@
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
-
 USE company_db;
 
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
 CREATE TABLE department (
-    id INT NOT NULL PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-    id INT NOT NULL PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    FOREIGN KEY (department)
-    REFERENCES department(id)
-    ON DELETE SET NULL
+    department_id INT
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    FOREIGN KEY (role)
-    REFERENCES role(id)
-    ON DELETE SET NULL
-    manager_id INT NOT NULL
-    FOREIGN KEY (employee)
-    REFERENCES employee(id)
-    ON DELETE SET NULL
+    role_id INT,
+    manager_id INT
 );
 
 
